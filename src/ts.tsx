4719204c0c6848e5b74d1f284cc84f99d786fbea
circle-gon <drew01547@gmail.com>
2022-06-08 17:33:21 -0400

    jsx testing

--- src/ts.tsx
+++ src/ts.tsx
@@ -2,7 +2,11 @@ import { defineComponent } from 'vue';
 
 const hi = defineComponent({
   render() {
-    return <div>Hi <p>hehehehhaw</p></div>;
+    return (
+      <div>
+        Hi <p>hehehehhaw</p> sus
+      </div>
+    );
   },
 });
 
